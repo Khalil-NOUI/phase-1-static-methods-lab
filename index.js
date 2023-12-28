925a4ff80d9c2918b09f4c8db5ecf18dc1df8c82
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-28 21:32:49 +0100

    Completed Assignments

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize (toCap) {
+    return (toCap[0].toUpperCase() + toCap.slice(1))
+  }
+
+  static sanitize(toSan) {
+    return (toSan.replace(/[^A-Za-z0-9-' ]+/g, ''))
+  }
+
+  static titleize(toTit) {
+
+    const words = toTit.split(" ")
+    console.log(words)
+   
+    let wrdArr = [];
+    wrdArr.push(words[0].charAt(0).toUpperCase() + words[0].slice(1))
+   
+    for (let i=1; i <= words.length - 1; i++) {
+      
+      const unWanted = ["a", "an", "but", "the", "of", "and", "for", "at", "by", "from"];
+      
+      if (!unWanted.includes(words[i])) {
+        wrdArr.push(words[i].charAt(0).toUpperCase() + words[i].slice(1))
+      } else {
+        wrdArr.push(words[i])
+      }
+  }
+
+  let result = wrdArr.join(" ")
+  return result
+  }
+
 }
